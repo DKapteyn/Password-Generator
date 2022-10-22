3fbf975ccece2bd5ce7977eb2440a7d7b64259b1
David Kapteyn <dave.kapteyn@gmail.com>
2022-10-22 15:21:08 -0600

    styled slider in all browsers

--- Components/Slider.tsx
+++ Components/Slider.tsx
@@ -3,8 +3,16 @@ import React, { useState } from "react";
 export default function Slider() {
   const [charNum, setCharNum] = useState(10);
 
+  const [calcWidth, setCalcWidth] = useState({ width: "50%" });
+
+  function setWidth(string: string) {
+    const charLength = parseInt(string) * 5;
+    setCalcWidth({ width: `${charLength}%` });
+  }
+
   function charChange(e: React.ChangeEvent<HTMLInputElement>) {
     setCharNum(parseInt(e.target.value));
+    setWidth(e.target.value);
   }
 
   return (
@@ -13,16 +21,22 @@ export default function Slider() {
         <h4 className="bodyS sm:body text-grey">Character Length</h4>
         <span className="headingM text-neonGreen ">{charNum}</span>
       </div>
-      <input
-        className=" slider accent-neonGreen  h-2 w-full "
-        type="range"
-        min="0"
-        max="20"
-        name="slider"
-        id="slider"
-        value={charNum}
-        onChange={charChange}
-      />
+      <div className="gridWrapper h-2 mt-7">
+        <input
+          className=" slider z-30  h-2 w-full "
+          type="range"
+          min="0"
+          max="20"
+          name="slider"
+          value={charNum}
+          onChange={charChange}
+        />
+        <span
+          style={calcWidth}
+          className={`h-2 bg-neonGreen mb-2 z-20  `}
+        ></span>
+        <span className="h-2 bg-veryDarkGrey w-full z-10 "></span>
+      </div>
     </div>
   );
 }
