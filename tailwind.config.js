53c5734f526b1d9f17022f09a31e1aa5a4ab0751
David Kapteyn <dave.kapteyn@gmail.com>
2022-11-10 10:26:48 -0700

    style changes to tailwind config reset next config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx}",
+    "./src/Components/**/*.tsx",
+    "./src/Components/**/*.ts",
   ],
   theme: {
     colors: {
