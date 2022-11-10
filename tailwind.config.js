bdea59acff2a44130d52646958b82f4a9c1681e0
David Kapteyn <dave.kapteyn@gmail.com>
2022-11-10 10:58:31 -0700

    changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,21 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        veryDarkGrey: "rgb(var(--veryDarkGrey)/<alpha-value>)",
-        darkGrey: "rgb(var(--darkGrey)/<alpha-value>)",
-        grey: "rgb(var(--grey)/<alpha-value>)",
-        almostWhite: "rgb(var(--almostWhite)/<alpha-value>)",
-        neonGreen: "rgb(var(--neonGreen)/<alpha-value>)",
-        red: "rgb(var(--red)/<alpha-value>)",
-        orange: "rgb(var(--orange)/<alpha-value>)",
-        yellow: "rgb(var(--yellow)/<alpha-value>)",
-      },
+    colors: {
+      veryDarkGrey: "rgb(var(--veryDarkGrey)/<alpha-value>)",
+      darkGrey: "rgb(var(--darkGrey)/<alpha-value>)",
+      grey: "rgb(var(--grey)/<alpha-value>)",
+      almostWhite: "rgb(var(--almostWhite)/<alpha-value>)",
+      neonGreen: "rgb(var(--neonGreen)/<alpha-value>)",
+      red: "rgb(var(--red)/<alpha-value>)",
+      orange: "rgb(var(--orange)/<alpha-value>)",
+      yellow: "rgb(var(--yellow)/<alpha-value>)",
     },
+
+    extend: {},
   },
   plugins: [],
 };
